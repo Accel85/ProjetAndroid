45f12dd2c52c2e682fa67d266309b52da37a9181
E184393C <axel.raballand@etu.univ-nantes.fr>
2020-03-17 12:17:10 +0100

    oui

--- app/src/main/java/com/example/projetandroid/Resultats.java
+++ app/src/main/java/com/example/projetandroid/Resultats.java
@@ -3,6 +3,10 @@ package com.example.projetandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class Resultats extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class Resultats extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultats);
+
+        String[] tab = {"oui","non","peut","etre","on","est","la","oui","non","peut","etre","on","est","la","oui","non","peut","etre","on","est","la","oui","non","peut","etre","on","est","la",};
+
+        ListView lv = findViewById(R.id.listV);
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,tab);
+        lv.setAdapter(aa);
     }
 }
