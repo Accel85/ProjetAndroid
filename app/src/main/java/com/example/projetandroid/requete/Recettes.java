48bd9211b642e19a60b454a5fa8dbe06438e114a
Your Name <you@example.com>
2020-03-24 11:29:36 +0100

    maj Recettes, init est opérationnel

--- app/src/main/java/com/example/projetandroid/requete/Recettes.java
+++ app/src/main/java/com/example/projetandroid/requete/Recettes.java
@@ -1,5 +1,13 @@
 package com.example.projetandroid.requete;
 
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import java.util.ArrayList;
 
 public class Recettes
@@ -24,5 +32,30 @@ public class Recettes
     public void removeRecette(Recette recette)
     {
         this.recettes.remove(recette);
+    }
+
+    public static Recettes init(String result)
+    {
+        Recettes recettes = new Recettes();
+
+        try {
+            JSONObject recipesObject = (JSONObject) new JSONTokener(result).nextValue();
+            JSONArray recipesArray = recipesObject.getJSONArray("results");
+
+            for (int i = 0; i < recipesArray.length(); i++)
+            {
+                JSONObject currentRecipe = recipesArray.getJSONObject(i);
+                Recette recipe = new Recette(
+                        currentRecipe.getInt("id"),
+                        currentRecipe.getString("title"),
+                        currentRecipe.getInt("readyInMinutes"),
+                        currentRecipe.getInt("servings"));
+                recettes.addRecette(recipe);
+            }
+        } catch (JSONException jsonE) {
+            Log.e("JSON error", "Error with recipes init");
+        }
+
+        return recettes;
     }
 }
