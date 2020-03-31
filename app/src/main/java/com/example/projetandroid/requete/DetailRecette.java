fe810401017f204fa76eef5b6c07b6af746ee5ed
E184393C <axel.raballand@etu.univ-nantes.fr>
2020-03-31 15:31:18 +0200

    test recettes

---
+++ app/src/main/java/com/example/projetandroid/requete/DetailRecette.java
@@ -0,0 +1,11 @@
+package com.example.projetandroid.requete;
+
+public class DetailRecette {
+    private String id;
+    private String title;
+    private String image;
+    private String servings;
+    private String readyInMinutes;
+    private String aggregateLikes;
+    private String analyzedInstructions;
+}
