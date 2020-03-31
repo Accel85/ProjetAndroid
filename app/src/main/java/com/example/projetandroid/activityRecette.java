f8c0aba07c087c75add7c6f0c6ba897a020e9285
Hugalaky <vnflk>
2020-03-31 15:29:38 +0200

    vue final 50 % affichage titre nombre de personne et temps d'une recette

--- app/src/main/java/com/example/projetandroid/activityRecette.java
+++ app/src/main/java/com/example/projetandroid/activityRecette.java
@@ -3,6 +3,12 @@ package com.example.projetandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.projetandroid.requete.Recette;
+import com.example.projetandroid.requete.Recettes;
+
+import java.util.ArrayList;
 
 public class activityRecette extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class activityRecette extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recette);
+
+        Bundle extras = getIntent().getExtras();
+
+        Recette test = extras.getParcelable("selected");
+
+        ArrayList list = extras.getParcelableArrayList("listeRecettes");
+
+
+        TextView titre = findViewById(R.id.tittle);
+        titre.setText(test.getTitle());
+
+        TextView temps = findViewById(R.id.temps);
+        temps.setText(test.getReadyIn()+"");
+
+        TextView nb = findViewById(R.id.nbPerso);
+        nb.setText(test.getServings()+"");
     }
 }
